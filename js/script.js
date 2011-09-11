675779eaa6e7f53cfaff3ecc1807e76f687b0674
Arnaud Leymet <arnaud.leymet@gmail.com>
2011-09-11 18:08:30 +0200

    Add some appearing effects to the title

--- js/script.js
+++ js/script.js
@@ -10,3 +10,12 @@ $('.enable').click(function() {
 $('#code').click(function() {
   $(this).focus().select();
 });
+
+function updateTitle(title) {
+  var t = title.split('.');
+  $('#title').attr('text-data', title.toLowerCase()).text(t[0]+'.').append($('<span>').text(t[1]));
+}
+updateTitle('tumblr.');
+setTimeout(function() {updateTitle('tumblr.b')}, 500);
+setTimeout(function() {updateTitle('tumblr.bo')}, 1000);
+setTimeout(function() {updateTitle('tumblr.box')}, 1500);
