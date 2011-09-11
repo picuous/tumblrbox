0a6a8655622d6c82425a498d7e38ba73527a71a5
Arnaud Leymet <arnaud.leymet@gmail.com>
2011-09-11 18:18:32 +0200

    And the title animation lives

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@
  * Author: Picuous
  */
 
+$('#title').click(function() {
+  $('#code_howto').fadeOut('fast', function() {
+    $('.enable').fadeIn('normal', runTitleAnimation);
+  });
+  return false;
+});
 $('.enable').click(function() {
   $(this).fadeOut('normal', function() {
     $('#code_howto').fadeIn();
@@ -15,9 +21,12 @@ function updateTitle(title) {
   var t = title.split('.');
   $('#title').attr('text-data', title.toLowerCase()).text(t[0]+'.').append($('<span>').text(t[1]));
 }
-setTimeout(function() {updateTitle('tumblr.bo')}, 500);
-setTimeout(function() {updateTitle('tumblr.b')}, 650);
-setTimeout(function() {updateTitle('tumblr.')}, 900);
-setTimeout(function() {updateTitle('tumblr.b')}, 1400);
-setTimeout(function() {updateTitle('tumblr.bo')}, 1650);
-setTimeout(function() {updateTitle('tumblr.box')}, 1900);
+function runTitleAnimation() {
+  setTimeout(function() {updateTitle('tumblr.bo')}, 500);
+  setTimeout(function() {updateTitle('tumblr.b')}, 650);
+  setTimeout(function() {updateTitle('tumblr.')}, 900);
+  setTimeout(function() {updateTitle('tumblr.b')}, 1400);
+  setTimeout(function() {updateTitle('tumblr.bo')}, 1650);
+  setTimeout(function() {updateTitle('tumblr.box')}, 1900);
+}
+runTitleAnimation();
