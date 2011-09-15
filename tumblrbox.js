738921d6eda7e2218a990904670019e6e1e34bda
Arnaud Leymet <arnaud.leymet@gmail.com>
2011-09-15 16:13:42 +0200

    Let the "powered by" mention to be displayed again

--- tumblrbox.js
+++ tumblrbox.js
@@ -81,7 +81,7 @@
           'showCloseButton': false,
           'hideOnContentClick': true,
           'onComplete': function() {
-            $('#fancybox-title').after('<a href="http://picuous.github.com/tumblrbox" target="_blank"><img src="picuous.github.com/tumblrbox/css/img/powered_by.png" alt="powered by Tumblrbox" /></a>');
+            $('#fancybox-title').after('<a id="powered_by_tumblrbox" href="http://picuous.github.com/tumblrbox" target="_blank"><img src="http://picuous.github.com/tumblrbox/css/img/powered_by.png" alt="powered by Tumblrbox" /></a>');
             setTimeout(function() { $('#powered_by_tumblrbox').fadeIn() }, 1000);
           }
         });
