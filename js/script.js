241089fd756bf50f3eeba7559ec837202790ba96
Arnaud Leymet <arnaud.leymet@gmail.com>
2011-09-16 18:01:53 +0200

    Track some events

--- js/script.js
+++ js/script.js
@@ -7,23 +7,31 @@ $('#title').click(function(e) {
     $('#presentation').fadeIn('normal', runTitleAnimation);
   });
   e.preventDefault();
+  _trackEvent('Plus', 'Logo', 'Click on the logo');
 });
 $('#enable').click(function() {
   $('#presentation').fadeOut('normal', function() {
     $('#code_howto').fadeIn();
   });
+  _trackEvent('Wizard', 'Enable', 'Click on the "Enable tumblrbox" button');
+});
+$('#code').click(function() {
+  $(this).focus().select();
+  _trackEvent('Wizard', 'Code', 'Click on the code area');
 });
 $('#done').click(function() {
   $('#code_howto').fadeOut('normal', function() {
     $('#further').fadeIn();
   });
-});
-$('#code').click(function() {
-  $(this).focus().select();
+  _trackEvent('Wizard', 'Done', 'Click on the "Im \'done" button');
 });
 $('#bookmarklet').click(function(e) {
   alert("In order for this bookmarklet to be usable:\n1. Add it to your favorites\n2. Use it on your tumblog");
   e.preventDefault();
+  _trackEvent('Plus', 'Bookmarklet', 'Click on the bookmarklet link');
+});
+$('#picuous').click(function() {
+  _trackEvent('Wizard', 'Picuous', 'Click on the Picuous button');
 });
 
 function updateTitle(title) {
